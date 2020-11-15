5034400d0710d16aac2359682b4628df2a8e6321
Ryan <lunaticwang726@gmail.com>
2020-11-15 17:01:25 +1100

    Ryan

--- lists and basic for python.py
+++ lists and basic for python.py
@@ -19,3 +19,37 @@ friends.sort()
 
 print(friends) # will return ['Franklin','Glenn','Sally'] , already be sorted
 
+# some built-in functions in lists
+nums = [2,41,12,9,74,15]
+print(len(nums))  # the numbers of nums
+print(max(nums)) # the max value
+print(min(nums)) # the min value
+print(sum(nums)) # the sum of the nums
+print(sum(nums) / len(nums)) # the average of nums
+
+# here are two examples for input numbers and average them 
+
+total = 0
+count = 0
+while True:
+    inp = input('enter a numer: ')
+    if inp == 'done':
+        break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+
+average = total / count # with out list
+
+# within list
+
+numlist = list()
+while True:
+    inp1 = input('enter a value: ')
+    if inp1 == 'done':
+        break
+    value = float(inp1)
+    numlist.append(value)
+
+average = sum(numlist) / len(numlist)
+
