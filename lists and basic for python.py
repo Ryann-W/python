48f9e88cd53e9c06fa0e6282918dad3494171cce
Ryan <lunaticwang726@gmail.com>
2020-11-15 16:47:35 +1100

    15/11/2020 By Ryan

--- lists and basic for python.py
+++ lists and basic for python.py
@@ -1,2 +1,21 @@
-iii\
-    
+# continue with list in python
+
+# append in list
+stuff = list()
+stuff.append('book')
+stuff.append(99)
+# stuff will retun ['book',99]
+# something in a list ?
+some = [4,5,6,7,8,9]
+
+9 in some # will return true
+10 in some # will return False
+15 not in some # will retun true
+
+# the sort method, a list can be sorted
+
+friends = ['Franklin','Sally','Glenn']
+friends.sort()
+
+print(friends) # will return ['Franklin','Glenn','Sally'] , already be sorted
+
