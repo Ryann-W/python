8e91d54ae66819470cacd9654c0cc4469443e732
Ryan <lunaticwang726@gmail.com>
2020-11-15 17:53:55 +1100

    Ryan

---
+++ Dict.py
@@ -0,0 +1,12 @@
+# the Dictionary of python
+# these definition originally from FreeCodeCamp
+# what is collection
+# A collection is nice because we can put more than one vlaue in it and canrry them all around in one convinent package
+# We have a bunch of values in a single'variable'
+# We do this by having more than one place'in' the variable
+# We have ways of finding the different places in the variables
+
+
+# List == A linear collection of values that stay in order
+# Dictionary == A 'bag' of values, each with its own label
+
