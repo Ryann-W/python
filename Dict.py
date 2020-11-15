0e3f00b5c48efcd21cdb9e54108835a0b99d948a
Ryan <lunaticwang726@gmail.com>
2020-11-15 18:12:04 +1100

    Ryan

--- Dict.py
+++ Dict.py
@@ -10,3 +10,39 @@
 # List == A linear collection of values that stay in order
 # Dictionary == A 'bag' of values, each with its own label
 
+# lists index their entries based on the position in the list
+# Dict are like bags , no order, so we index the things we put in the dictionary with a 'look up bag'
+
+purse = dict() # setup a blank dict
+purse['Money'] = 10
+purse['candy'] = 3
+purse['tissues'] = 75
+print(purse) # it will return {'Money': 10, 'candy': 3, 'tissues': 75}
+purse['candy']  = purse['candy'] + 2
+print(purse) # {'Money': 10, 'candy': 5, 'tissues': 75}
+
+# the comparision of Lists and Dictionaries
+# list
+list = list()
+list.append(1)
+list.append(2)
+list.append(3)
+
+# dict
+dict = dict()
+dict['age'] = 12
+dict['course']  = 182
+
+print(list) # return [1, 2, 3]
+print(dict) # return {'age': 12, 'course': 182}
+
+#          list                      Dict
+#  key             value     key              value 
+#  [0]               1       age                12
+#  [1]               2       course             182
+
+# make an empty dictionary using empty curly braces
+pp = { }
+bb = {'a':1, 'b':2, 'c':3}
+print(type(pp))
+print(dir(pp))
