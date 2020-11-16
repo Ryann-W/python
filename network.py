b7262c0fb2d1f318343328c942bc2f4806a45d53
Ryan <lunaticwang726@gmail.com>
2020-11-16 13:56:10 +1100

    Ryan

---
+++ network.py
@@ -0,0 +1,33 @@
+
+# python for network basic
+
+
+# TCP -- transport control protocol
+# IP -- internet protocol
+
+
+# from the FreeCodeCamp : in computer networking, an interent socket or network socket is an endpoint of a bidirectional(双向的)
+#inter-process communication flow across an internet protocol-based computer network, such as the internet
+
+# TCP Port Numbers
+# a port is an application-specific or process-specific sofrware communications endpoint
+# it allows multiple networked applications to coexist on the same server
+
+# common tcp ports
+
+Telent(23) --Login
+SSH(22) --Secure Login
+HTTP(80) (HyperText Transfer Protocol)
+HTTPS(443)-Secure
+SMTP(25)--Mail
+IMAP(143/220/993) -Mail Retrieval
+POP(109/110) -Mail Retrieval
+DNS(53) - Domain Name
+FTP(21) - File Transfer
+
+# the Protocol from FreeCodeCamp
+
+# a set of rules that all parties follow so we can predict each other's behavior
+# and not bump into each other
+#   On two-way roads in USA, drive on the right-hand side of the road
+#   On two-way roads in the UK, drive on the left-hand side of the road
