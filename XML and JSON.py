df875c7bd16533abfcc1718da17841f26cc04f4f
Ryan <lunaticwang726@gmail.com>
2020-11-16 16:45:59 +1100

    Ryan

---
+++ XML and JSON.py
@@ -0,0 +1,19 @@
+
+# XML eXtensible Markup Language (XML)
+# Note from FreeCodeCamp
+# Primary purpose is to help information systems share structured data
+# It started as a simplified subset of the Standard Generalized Markup Language(SGML), and is designed to be relatively
+# human-legible
+
+# Tags indicate the beginning and ending of elements
+# Attributes - Keywords/value pairs on the opening tag of XML
+# Serialize/ De-Serialize - Convert data in one program into a common format that can be storted and/or transmitted between systems
+# in a programming language-independent manner
+
+# Start Tag              <person>
+# End Tag                     <name>Chuck</name>
+# Text Content                <phone type="intl">
+#                                 +1 734 303 4456
+# Attributes                  </phone>
+#                             <email hide="yes" />
+# Self Closing Tag        </person>
