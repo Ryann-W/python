ca4802f27f82590f7f6f10f6d6f68a359a5101f2
Ryan <lunaticwang726@gmail.com>
2020-11-17 12:26:04 +1100

    Ryan

--- OO.py
+++ OO.py
@@ -79,10 +79,25 @@ s.parties()   # the value return 2 bacause s.parties already have 1, and not bec
 
 # Some definitions of class reference from FreeCodeCamp
 
-# Class - a template
+# Class - a template (a shape of a thing)
 # Attribute - A variable within a class
 # Method - A function within a class
 # Object - A particular instance of a class
 # Constructor - Code that runs when an object is created
 # Inheritance - The ability to extend a class to make a new class
 
+# the new class(child) has all the capabilities of the old class(parent) - and then some more
+
+# one example
+
+class FootballFun(PartyAnimals):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.parties()
+        print(self.name, "points", self.points)
+
+
+g = FootballFun("Mij")
+g.parties()
+g.touchdown()
