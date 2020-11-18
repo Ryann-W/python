c17059dda380b625eec5291374042693a53793f5
Ryan <lunaticwang726@gmail.com>
2020-11-18 16:23:21 +1100

    Ryan

--- practice.py
+++ practice.py
@@ -6,7 +6,7 @@
 # Once 'done' is entered, print out the largest and smallest of the numbers. 
 # If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
 # Enter 7, 2, bob, 10, and 4 and match the output below.
-
+'''
 largest = None
 smallest = None
 lst = list()
@@ -46,11 +46,35 @@ for letter in 'banana' :
     print(letter)
 
 
+'''
 
-fname = input('Enter the file name: ')
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. 
+# Do not use the sum() function or a variable named sum in your solution.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt 
+# when you are testing below enter mbox-short.txt as the file name.
 
-file = open(fanme)
-for line in file:
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue
-    print(line)
+
+
+fname = input("Enter file name: ")
+fh = open(fname)
+total = 0
+count = 0
+middle = list()
+final = list()
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        print(line)
+        middle = line.split()
+        final.append(float(middle[1]))
+
+for value in final:
+    total = total + value
+    count = count + 1
+
+average = total / count
+print(average)
+
+print("Done")
+
