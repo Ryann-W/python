8de6114e005e420da2aa5c7eb587043b67b613d9
Ryan <lunaticwang726@gmail.com>
2020-11-20 09:11:38 +1100

    Ryan

--- practice2.py
+++ practice2.py
@@ -3,12 +3,12 @@
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
-
+import json
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-
+'''
 url = 'http://py4e-data.dr-chuck.net/comments_1071074.xml'
 
 uh = urllib.request.urlopen(url, context=ctx)
@@ -26,8 +26,26 @@ total = 0
 for count in counts:
     total = total + int(count.text)
 print(total)
-
+'''
 # --------------------  extract XML  ------------------
 
 
+url_json = 'http://py4e-data.dr-chuck.net/comments_1071075.json'
 
+uh_json = urllib.request.urlopen(url_json,context = ctx)
+
+
+data_json = json.loads(uh_json.read())
+
+# print(data_json)
+
+extract_data = data_json.get('comments',None)
+
+# print(extract_data)
+
+total = 0
+
+for item in extract_data:
+    total = total + int(item.get('count',None))
+
+print(total)
