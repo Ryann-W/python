73a97d5528d8e9cc3311a90280ffd1b9a9fe2867
Ryan <lunaticwang726@gmail.com>
2020-11-20 08:57:09 +1100

    Ryan

--- practice2.py
+++ practice2.py
@@ -1,4 +1,6 @@
-import urllib.request, urllib.parse, urllib.error
+# --------------------  extract XML  ------------------
+
+mport urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
@@ -7,28 +9,25 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+url = 'http://py4e-data.dr-chuck.net/comments_1071074.xml'
 
 uh = urllib.request.urlopen(url, context=ctx)
 
 data = uh.read()
 
-# print(data.decode())
+print(data.decode())
 
 tree = ET.fromstring(data)
 
-results = tree.findall('comments')
-print(results)
-#name = results[0].find('name').text
-#count = results[0].find('count').text
-#name = tree.find('name').text
-#count = tree.find('count').text
-#print(results)
+counts = tree.findall('.//count')
 
-#print("this is the name",name)
-#print("this is the count",count)
+total = 0
+
+for count in counts:
+    total = total + int(count.text)
+print(total)
+
+# --------------------  extract XML  ------------------
 
 
-# name = results[0].find('comment').find('name').text
-# print(name)
 
